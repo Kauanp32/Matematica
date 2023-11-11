550e37639ceec43e605d85998fffa30a5b3faf9b
kauan pereira <132390158+Kauanp32@users.noreply.github.com>
2023-11-11 10:28:03 -0300

    Update Adicao.java

--- Adicao.java
+++ Adicao.java
@@ -1,19 +1,24 @@
 package Matematica;
 
-
 import java.util.Scanner;
 
 public class Adicao implements Operacao {
-	Scanner sc = new Scanner(System.in);
 
-	@Override
-	public double calcular(double num1, double num2) {
-		System.out.println("Digite o primeiro numero da Adição: ");
-		num1 = sc.nextDouble();
-		System.out.println("Digite o segundo numero da Adição: ");
-		num2 = sc.nextDouble();
-		
-		return num1 + num2;
-	}
+    Scanner sc = new Scanner(System.in);
 
+    @Override
+    public double calcular(double num1, double num2) {
+        System.out.println("Digite o primeiro numero da Adição: ");
+        num1 = sc.nextDouble();
+        System.out.println("Digite o segundo numero da Adição: ");
+        num2 = sc.nextDouble();
+
+        return num1 + num2;
+    }
+        public static void main(String[] args) {
+            Adicao adicao = new Adicao();
+            double resultado = adicao.calcular(0, 0);
+            System.out.println("Resultado da Adição: " + resultado);
+        }
+    
 }
